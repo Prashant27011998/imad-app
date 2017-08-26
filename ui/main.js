0c833120969626e58810fe276af565cfad4dcd23
Prashant27011998 <30834916+Prashant27011998@users.noreply.github.com>
2017-08-26 23:57:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,6 @@ element.innerHTML = "New Value";
 //move the image
 var img = document.getElementById("prashant");
 img.onclick = function(){
-    img.style.marginLeft="100px";
+    img.style.marginLeft="180px";
     
 };
