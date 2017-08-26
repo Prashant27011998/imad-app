ea2f14b318248427acf3c174802ea68ca81d909f
Prashant27011998 <30834916+Prashant27011998@users.noreply.github.com>
2017-08-27 01:34:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,2 @@
 console.log('Loaded!');
-var element = document.getElementById("main-text");
-element.innerHTML = "";
-//move the image
-var img = document.getElementById("prashant");
-var marginLeft = 0;
-function moveRight()
-{
-    marginLeft = marginLeft + 1 ;
-img.style.marginLeft = marginLeft + "px";
-}
-img.onclick = function(){
-     var interval=setInterval(moveRight, 50);
-    };
+var counter = function()
