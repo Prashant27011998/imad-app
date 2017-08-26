d9aa896f49f7ed8ec38db05c09afd2ac1a1ce99c
Prashant27011998 <30834916+Prashant27011998@users.noreply.github.com>
2017-08-27 00:16:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,8 +5,10 @@ element.innerHTML = "New Value";
 var img = document.getElementById("prashant");
 var marginLeft=0;
 function moveright()
-marginLeft= marginLeft + 5 ;
+{
+    marginLeft= marginLeft + 5 ;
 image.style.marginLeft =marginLeft+ "px";
+}
 img.onclick = function(){
     
     var interval=setInterval(moveRight, 50);
